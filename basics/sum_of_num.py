4edc4ada2adccabc1617c749aa9e024994634703
JacinthaChristabel <jacinthachristabel@gmail.com>
2026-07-08 12:08:02 +0530

    sum of numbers

---
+++ basics/sum_of_num.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a: "))
+b=int(input("Enter b: "))
+c=int(input("Enter c: "))
+
+# Approach 1: Store in variable first
+sum=a+b
+print(f"sum of 2 numbers is {sum}")
+
+# Approach 2: Calculate directly in f-string
+print(f"sum of 3 numbers is {a+b+c}")
+
