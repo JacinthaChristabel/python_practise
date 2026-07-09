71906d5155f5760bbb57a48e71516373ae5868e4
JacinthaChristabel <jacinthachristabel@gmail.com>
2026-07-09 23:12:49 +0530

    arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,9 @@
+a,b=map(int, input("Enter 2 numbers with space bewteen them: ").split())
+
+print(f"addition of two numbers is {a+b}")
+print(f"Difference of two numbers is {a-b}")
+print(f"Product of two numbers is {a*b}")
+print(f"remainder of a/b is {a%b}")
+print(f"Quotient of a/b is {a//b}")
+print(f"square of a is {a**2} and square of b is {b**2}")
+print(f"cube of a is {a**3} and cube of b is {b**3}")
